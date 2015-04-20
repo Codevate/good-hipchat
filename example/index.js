6757d8dfbf631730f7db81ebd7f207b5045be7bf
Greg Allen <me@jga.me>
2015-04-20 15:52:05 -0700

    update to support Good 6

--- example/index.js
+++ example/index.js
@@ -6,41 +6,40 @@ var options = {
   opsInterval: 1000,
   reporters: [{
     reporter: require('../'),
-    args:[
-      {
-        auth_token: '1234',
-        room: 'Devs',
-        roomToken: '1234',
-        debug: true,
-        events: {
+    events: {
+      error: '*',
+      log: ['example', 'error']
+    },
+    config: {
+      authToken: 'daaf19ba00ad0953b546dec76a2761',
+      roomToken: 'TjyIN1cvptamObk4sKf8ZXyQtd0AnoiIgKhboB0K',
+      room: 'Test',
+      customizeEvents: {
+        'error': {
+          '*': {
+            color: 'red',
+            notify: true,
+            format: function(event) {
+              return event.error.toString() + ' Details: http://example.com?e=something';
+            }
+          }
+        },
+        'log': {
+          'example': {
+            color: 'yellow',
+            format: function(event) {
+              return event.data;
+            }
+          },
           'error': {
-            '*': {
-              color: 'red',
-              room: 'Emergency Ops',
-              roomToken: '56w34',
-              notify: true,
-              format: function(event) {
-                return event.error.toString() + ' Details: http://example.com?e=something';
-              }
+            color: 'red',
+            format: function(event) {
+              return 'ERROR: '+ event.data;
             }
-          },
-          'log': {
-            'example': {
-              color: 'yellow',
-              format: function(event) {
-                return event.data;
-              }
-            },
-            'error': {
-              color: 'red',
-              format: function(event) {
-                return event.data;
-              }
-            }
-          },
-        }
+          }
+        },
       }
-    ]
+    }
   }]
 };
 
@@ -52,19 +51,31 @@ server.register({
     console.error(err);
   } else {
     server.start(function () {
-      console.info('Server started at ' + server.info.uri);
+      server.log(['info'], 'Server started at ' + server.info.uri);
     });
   }
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: function(request, reply) {
-      server.log(['error', 'example'], 'Should only display once');
-      server.log(['example'], 'Example again');
-      server.log(['not-used'], 'This shouldn\'t be logged');
-      server.log(['social-meta', 'twitter', 'error'], { url: 'url', error: new Error('something') });
-      reply(new Error('something'));
-    }
-  });
+  server.route([{
+      method: 'GET',
+      path: '/',
+      handler: function(request, reply) {
+        server.log(['example'], 'Example');
+        reply('ok');
+      }
+    }, {
+      method: 'GET',
+      path: '/error-example',
+      handler: function(request, reply) {
+        server.log(['error', 'example'], 'Should only display once');
+        reply('ok');
+      }
+    }, {
+      method: 'GET',
+      path: '/error',
+      handler: function(request, reply) {
+        reply(new Error('ooops'));
+      }
+    },
+
+  ]);
 });
--- index.js
+++ index.js
@@ -1,90 +1,85 @@
-var GoodReporter = require('good-reporter');
 var Hipchatter = require('hipchatter');
-var _ = require('lodash-node');
+var _ = require('lodash');
 var Hoek = require('hoek');
-var chalk = require('chalk');
+var Squeeze = require('good-squeeze').Squeeze;
 
 var defaults = {
   color: 'yellow',
   notify: false,
-  debug: false,
   format: function(event) {
-    if(event.message) {
+    if (event.data && typeof event.data == 'string') {
+      return event.data;
+    } else if (event.message) {
       return event.message;
-    } else if(event.error) {
+    } else if (event.error) {
       return event.error.toString();
     } else {
-      return event;
+      return JSON.stringify(event);
     }
-  },
-  events: {}
+  }
 };
 
-var GoodHipchat = function(options) {
+var GoodHipchat = function(events, config) {
 
-  this.options = Hoek.clone(options);
-
-  this.options = Hoek.applyToDefaults(defaults, this.options);
-
-  var events = {};
-
-  for(var event in this.options.events) {
-    if(this.options.events[event]['*']) {
-      events[event] = '*';
-      continue;
-    }
-
-    events[event] = Object.keys(this.options.events[event]);
+  if (!(this instanceof GoodHipchat)) {
+    return new GoodHipchat(events, config);
   }
 
-  this.reporter = new GoodReporter(events);
+  config = config || {};
+  Hoek.assert(config.authToken, 'config.authToken must exist');
+  Hoek.assert(config.roomToken, 'config.roomToken must exist');
+  Hoek.assert(config.room, 'config.room must exist');
 
-  this.reporter._report = this._report.bind(this);
-  this.reporter.start = this.start.bind(this);
-  this.reporter.stop = this.stop.bind(this);
+  var settings = Hoek.applyToDefaults(defaults, config);
+
+  this._streams = {
+    squeeze: Squeeze(events)
+  };
+  this._eventQueue = [];
+  this._settings = settings;
+
 };
 
-GoodHipchat.prototype.start = function(emitter, callback) {
-  emitter.on('report', this.reporter._handleEvent.bind(this.reporter));
+GoodHipchat.prototype.init = function(stream, emitter, callback) {
 
-  this.hipchat = new Hipchatter(this.options.auth_token);
+  var self = this;
+  this.hipchat = new Hipchatter(this._settings.authToken);
+
+  this._streams.squeeze.on('data', function (data) {
+    self.report(data);
+  });
+
+  stream.pipe(this._streams.squeeze);
+
 
   return callback();
 };
 
-GoodHipchat.prototype.stop = function(){};
-
-GoodHipchat.prototype._report = function(event, eventData) {
-  var eventConfig = this.options.events[event];
+GoodHipchat.prototype.report = function(eventData) {
+  var eventConfig = this._settings.customizeEvents[eventData.event] || {};
   var tagConfig = {};
 
-  if(eventConfig['*']) {
+  if (eventConfig['*']) {
     tagConfig = eventConfig['*'];
   } else {
     _.each(eventData.tags, function(tag) {
-      if (typeof eventConfig[tag] !== undefined) {
+      if (eventConfig[tag]) {
         tagConfig = eventConfig[tag];
       }
     });
   }
-
   var message = {
-    token: tagConfig.roomToken || this.options.roomToken,
-    message: tagConfig.format ? tagConfig.format(eventData) : this.options.format(eventData),
-    notify: (typeof tagConfig.notify !== 'undefined') ? tagConfig.notify : this.options.notify,
-    color: tagConfig.color || this.options.color
+    token: this._settings.roomToken,
+    message: tagConfig.format ? tagConfig.format(eventData) : this._settings.format(eventData),
+    notify: (typeof tagConfig.notify !== 'undefined') ? tagConfig.notify : this._settings.notify,
+    color: tagConfig.color || this._settings.color
   };
 
-  if(this.options.debug) {
-    console.log(chalk.bold[message.color]('Message to room:', tagConfig.room || this.options.room));
-    console.log(message);
-  } else {
-    this.hipchat.notify(tagConfig.room || this.options.room, message, function(err) {
-      if(err) {
-        console.log('Error sending message to room', err);
-      }
-    });
-  }
+  this.hipchat.notify(this._settings.room, message, function(err) {
+    if(err) {
+      console.log('Error sending message to room', err);
+    }
+  });
 };
 
 module.exports = GoodHipchat;
